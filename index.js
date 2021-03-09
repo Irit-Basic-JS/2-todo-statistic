0e985599540c117d27e93fa01ff1e3a3f1afc3dd
Egor1712 <58880149+Egor1712@users.noreply.github.com>
2021-03-09 19:31:36 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 const {getAllFilePathsWithExtension, readFile} = require("./fileSystem");
 const {readLine} = require("./console");
 const pattern = /\/\/ TODO(([\w\s\d]+);)?\s?((\d{0,4}-\d{0,2}-\d{0,2});)?\s?(.+)?/gi;
@@ -10,7 +12,12 @@ readLine(processCommand);
 
 function getFiles() {
 	const filePaths = getAllFilePathsWithExtension(process.cwd(), "js");
-	return filePaths.map(path => readFile(path));
+	return filePaths.map(path1 => {
+		return {
+			file: readFile(path1),
+			path: path.basename(path1),
+		};
+	});
 }
 
 
@@ -21,22 +28,25 @@ function processCommand(command) {
 			process.exit(0);
 			break;
 		case "show":
-			console.log(comments.map(x => x.value));
+			writeComments(comments);
 			break;
 		case "important":
-			console.log(comments
-			.filter(x => x.importance !== 0)
-			.map(x => x.value));
+			writeComments(comments
+			.filter(x => x.importance !== 0));
 			break;
 		case "user":
 			let name = commands[1].toLowerCase();
-			console.log(comments
-			.filter(x => x.name === name)
-			.map(x => x.value));
+			writeComments(comments
+			.filter(x => x.name === name));
 			break;
 		case "sort":
 			let parameter = commands[1];
-			console.log(getSortedCommentsByParameter(parameter, comments).map(x => x.value));
+			writeComments(getSortedCommentsByParameter(parameter, comments));
+			break;
+		case "date":
+			let date = new Date(commands[1]);
+			writeComments(comments
+			.filter(x => x.date > date));
 			break;
 		default:
 			console.log("wrong command");
@@ -47,15 +57,16 @@ function processCommand(command) {
 function parseAllCommentsFromFiles(files) {
 	const result = [];
 	for (const file of files) {
-		const matches = file.matchAll(pattern);
+		const matches = file.file.matchAll(pattern);
 		for (const match of matches) {
 			if (match) {
 				result.push({
 					value: match[0],
-					name: match[2]?.toLowerCase(),
-					date: Date.parse(match[4]),
-					comment: match[5],
+					name: match[2]?.trim().toLowerCase(),
+					date: new Date(match[4]),
+					comment: match[5].trim(),
 					importance: count(match[5], "!"),
+					path: file.path,
 				});
 			}
 		}
@@ -99,7 +110,7 @@ function sortByName(a, b) {
 }
 
 function sortByDate(a, b) {
-	if (!a.date)
+	if (isNaN(a.date))
 		return 1;
 	if (a.date < b.date)
 		return 1;
@@ -107,4 +118,45 @@ function sortByDate(a, b) {
 }
 
 
+function writeComments(comments) {
+	let header = decorateString({
+		name: "name",
+		date: undefined,
+		comment: "comment",
+		importance: 1,
+		path: "path",
+	});
+	console.log(header);
+	console.log("-".repeat(header.length));
+	for (const comment of comments) {
+		console.log(decorateString(comment));
+	}
+	console.log("-".repeat(header.length));
+}
+
+function formatStr(str, length) {
+	if (str === undefined)
+		return "".padEnd(10);
+	if (str.length > length)
+		return `${str.slice(0, length - 3)}...`;
+	return str.padEnd(length);
+}
+
 // TODO you can do it!
+
+function formatDate(date) {
+	if (!(date instanceof Date))
+		return date;
+	if (isNaN(date))
+		return "";
+	let day = date.getDate().toString().padStart(2, "0");
+	let month = (date.getMonth() + 1).toString().padStart(2, "0");
+	let year = date.getFullYear();
+	return `${day}-${month}-${year}`;
+}
+
+function decorateString(comment, indentLength = 2) {
+	const isImportant = comment.importance !== 0;
+	const indent = " ".repeat(indentLength);
+	return `${isImportant ? "!" : " "}${indent}|${indent}${formatStr(comment.name, 10)}${indent}|${indent}${formatStr(formatDate(comment.date), 10)}${indent}|${indent}${formatStr(comment.comment, 50)}${indent}|${indent}${formatStr(comment.path, 20)}`;
+}

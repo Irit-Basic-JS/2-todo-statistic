b48a0774adc11454d4cad9e055d630b69c28f92d
FreeTrally <39124135+FreeTrally@users.noreply.github.com>
2021-03-12 23:14:02 +0500

    End

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const files = getFiles();
 const todos = getTodos();
 
 console.log('Please, write your command!');
-// readLine(processCommand);
-processCommand('sort user');
+// readLine(processCommand); Idk how to use it
+processCommand('sort date');
 
 function getFiles() {
     const filePaths = getAllFilePathsWithExtension(process.cwd(), 'js');
@@ -66,29 +66,41 @@ function getTodos() {
 }
 
 function getImportant(){
+    let results = [];
+
     for (let todo of todos){
         if (todo.includes("!")){
-            console.log(todo);
+            results.push(todo);
         }
+    }
+
+    for (let row of getTable(results)){
+        console.log(row);
     }
 }
 
 function getUserTodos(user){
     let user1 = user.charAt(0).toLowerCase() + user.slice(1);
     let user2 = user.charAt(0).toUpperCase() + user.slice(1);
+    let results = [];
+
     for (let todo of todos){
         if (todo.includes('// TODO ' + user1) 
         || todo.includes('// TODO ' + user2)){
-            console.log(todo);
+            results.push(todo);
         }
+    }
+
+    for (let row of getTable(results)){
+        console.log(row);
     }
 }
 
 function sortImp(){
     let clone = todos.slice();
     clone.sort((a, b) => compareImp(a, b));
-    for (let todo of clone){
-        console.log(todo);
+    for (let row of getTable(clone)){
+        console.log(row);
     }
 }
 
@@ -105,8 +117,8 @@ function compareImp(a, b) {
 function sortUser(){
     let clone = todos.slice();
     clone.sort((a, b) => compareUser(a, b));
-    for (let todo of clone){
-        console.log(todo);
+    for (let row of getTable(clone)){
+        console.log(row);
     }
 }
 
@@ -126,27 +138,106 @@ function getUser(todo){
 
 function sortDate(){
     let clone = todos.slice();
+
     clone.sort((a, b) => compareDate(a, b));
-    for (let todo of clone){
-        console.log(todo);
+
+    for (let row of getTable(clone)){
+        console.log(row);
     }
 }
 
 function compareDate(a, b) {
     let aS = getDate(a);
     let bS = getDate(b);
-    return aS.localeCompare(bS);
+    return bS.localeCompare(aS);
 }
 
 function getDate(todo){
-    let date = [];
     if ((todo.match(new RegExp("-", "g")) || []).length > 1){
         return todo.substring(todo.indexOf('-') - 4, todo.indexOf('-') + 6);
     } else {
-        return '9999-99-99';
+        return '0000-00-00';
     }
 }
 
-// function getAfterDate(date)
+function getAfterDate(date){
+    let results = [];
+
+    while (date.length < 10){
+        date = date.concat('-00');
+    }
+    let clone = todos.slice();
+    for (let todo of clone){
+        if (getDate(todo).localeCompare(date) > 0){
+            results.push(todo);
+        }
+    }
+
+    for (let row of getTable(results)){
+        console.log(row);
+    }
+}
+
+function getTable(tableData) {
+    let allTodoData = [];
+    let rowLength = [4, 4, 7];
+
+    for (let todo of tableData){
+        let todoData = getTodoData(todo);
+        allTodoData.push(todoData);
+        rowLength[0] = Math.max(rowLength[0], todoData[0].length);
+        rowLength[1] = Math.max(rowLength[1], todoData[1].length);
+        rowLength[2] = Math.max(rowLength[2], todoData[2].length);
+    }
+
+    rowLength[0] = Math.min(rowLength[0], 10);
+    rowLength[1] = Math.min(rowLength[1], 10);
+    rowLength[2] = Math.min(rowLength[2], 50);
+ 
+    let separ = "  |  ";
+
+    let table = [];
+    let title = "!" + separ + cutField("user", rowLength[0]) + separ 
+    + cutField("date", rowLength[1]) + separ + cutField("comment", rowLength[2]);
+    let border = '-'.repeat(title.length);
+    table.push(title, border);
+
+    for (let todoData of allTodoData){
+        let fields = [todoData[2].includes('!') ? '!' : ' '];
+        fields.push(cutField(todoData[0], rowLength[0]));
+        fields.push(cutField(todoData[1], rowLength[1]));
+        fields.push(cutField(todoData[2], rowLength[2]));
+        table.push(fields.join(separ));
+    }
+
+    table.push(border);
+    return table;
+}
+
+function cutField (str, len) {
+    return str.length > len ? str.slice(0, len-3) + "..." 
+    : str + ' '.repeat(len - str.length);
+}
+
+function getTodoData(todo){
+    let data = ['', '', ''];
+    
+    if (todo.includes(';')){
+        data[0] = todo.substring(8, todo.indexOf(';'));
+    } else {
+        data[2] = todo.substring(8);
+        return data;
+    }
+
+    if (todo.includes('-')){
+        data[1] = todo.substring(todo.indexOf('-') - 4, todo.indexOf('-') + 6);
+    } else {
+        data[2] = todo.substring(todo.indexOf(';') + 1);
+        return data;
+    }
+
+    data[2] = todo.substring(todo.indexOf('-') + 8);
+    return data;
+}
 
 // TODO you can do it!

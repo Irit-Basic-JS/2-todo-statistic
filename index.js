8f7ddfd437945c8649889c1d4764e0e40688e97e
Евгений Куликов <foxkait@icloud.com>
2021-03-14 16:11:04 +0500

    1

--- index.js
+++ index.js
@@ -1,25 +1,46 @@
 const {getAllFilePathsWithExtension, readFile} = require('./fileSystem');
 const {readLine} = require('./console');
 
+const startOfComment = "// TODO ";
 const files = getFiles();
+const comments = getComments(files);
+console.log(comments);
 
 console.log('Please, write your command!');
 readLine(processCommand);
 
 function getFiles() {
-    const filePaths = getAllFilePathsWithExtension(process.cwd(), 'js');
-    return filePaths.map(path => readFile(path));
+	const filePaths = getAllFilePathsWithExtension(process.cwd(), 'js');
+	return filePaths.map(path => readFile(path));
 }
 
 function processCommand(command) {
-    switch (command) {
-        case 'exit':
-            process.exit(0);
-            break;
-        default:
-            console.log('wrong command');
-            break;
-    }
+	switch (command) {
+		case 'exit':
+			process.exit(0);
+			break;
+		default:
+			console.log('wrong command');
+			break;
+	}
+}
+
+function getComments(files) {
+	let comments = [];
+	for (const file of files) {
+		let lines = file.split('\r\n');
+		for (let line of lines) {
+			let comment = getComment(line, startOfComment);
+			if (comment) comments.push(comment);
+		}
+	}
+	return comments;
+}
+
+function getComment(line, startOfComment) {
+	let index = line.lastIndexOf(startOfComment);
+	if (~index && !['"', "'", '`'].includes(line[index - 1]))
+		return line.slice(index + startOfComment.length);
 }
 
 // TODO you can do it!

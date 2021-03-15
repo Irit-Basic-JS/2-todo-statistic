19d46a60d571b8088e074a3f67c57e49047c81db
Евгений Куликов <foxkait@icloud.com>
2021-03-15 11:45:23 +0500

    1-10 finally (+красивая таблица) + little refactoring

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const {readLine} = require('./console');
 
 const startOfComment = "// TODO ";
 const filesAndPath = getFilesAndPath();
-const comments = getComments(filesAndPath);
+const commentsInfo = getCommentsInfo(filesAndPath);
 
 console.log('Please, write your command!');
 readLine(processCommand);
@@ -15,14 +15,14 @@ function getFilesAndPath() {
 	});
 }
 
-function getComments(filesAndPath) {
+function getCommentsInfo(filesAndPath) {
 	return filesAndPath.map(fileAndPath => fileAndPath.file.split('\r\n')
-		.map(line => getComment(line, startOfComment, fileAndPath.path))
+		.map(line => getCommentInfo(line, startOfComment, fileAndPath.path))
 		.filter(line => line))
 	.flat(1);
 }
 
-function getComment(line, startOfComment, path) {
+function getCommentInfo(line, startOfComment, path) {
 	let index = line.lastIndexOf(startOfComment);
 	if (~index && !['"', "'", '`'].includes(line[index - 1])) {
 		let comment = line.slice(index + startOfComment.length);
@@ -30,7 +30,7 @@ function getComment(line, startOfComment, path) {
 		let split = comment.split(';', 3).map(line => line.trim());
 		let fileName = path.slice(path.lastIndexOf("/") + 1);
 		return (split.length === 3)
-			? {important, name: split[0], dateOfCreation: new Date(split[1]), comment: split[2], fileName: fileName}
+			? {important, name: split[0], dateOfCreation: new Date(split[1]), comment: split[2], fileName}
 			: {important, comment, fileName};
 	}
 }
@@ -70,26 +70,26 @@ function processCommand(command) {
 			process.exit(0);
 			break;
 		case 'show':
-			result = commands.show(comments);
+			result = commands.show(commentsInfo);
 			break;
 		case 'important':
-			result = commands.important(comments);
+			result = commands.important(commentsInfo);
 			break;
 		case 'sort importance':
-			result = commands.sortImportance(comments);
+			result = commands.sortImportance(commentsInfo);
 			break;
 		case 'sort user':
-			result = commands.sortUser(comments);
+			result = commands.sortUser(commentsInfo);
 			break;
 		case 'sort date':
-			result = commands.sortDate(comments);
+			result = commands.sortDate(commentsInfo);
 			break;
 		default:
 			let split = command.split(' ');
 			if (split[0] === 'user')
-				result = commands.user(comments, split[1]);
+				result = commands.user(commentsInfo, split[1]);
 			else if (split[0] === 'date')
-				result = commands.date(comments, split[1]);
+				result = commands.date(commentsInfo, split[1]);
 			else
 				console.log('wrong command');
 			break;
@@ -100,61 +100,66 @@ function processCommand(command) {
 }
 
 function tableParser(data) {
-	let tableSizes = getTableSizes(data);
-	createSeparatorLine(tableSizes);
-	createTitle(tableSizes);
-	createSeparatorLine(tableSizes);
-	data.forEach(record => console.log(getLine(record, tableSizes)));
-	createSeparatorLine(tableSizes);
+	let tableSize = getTableSize(data);
+	createSeparatorLine(tableSize);
+	createTitle(tableSize);
+	createSeparatorLine(tableSize);
+	data.forEach(record => console.log(getLine(record, tableSize)));
+	createSeparatorLine(tableSize);
 }
 
-function getLine(record, tableSizes) {
-	let paragraph = '  | ';
-	let indentation = '  |';
-	return paragraph + [
+function getLine(record, tableSize) {
+	let prefix = '  | ';
+	let ending = '  |';
+	let root = [
 		record.important ? '!' : ' ',
-		getCell(record.name, tableSizes[1]),
-		getCell(record.dateOfCreation, tableSizes[2]),
-		getCell(record.comment, tableSizes[3]),
-		getCell(record.fileName, tableSizes[4])
-	].join(' | ') + indentation;
+		getCell(record.name, tableSize.name),
+		getCell(record.dateOfCreation, tableSize.dateOfCreation),
+		getCell(record.comment, tableSize.comment),
+		getCell(record.fileName, tableSize.fileName)
+	].join(' | ');
+
+	return `${prefix}${root}${ending}`;
 }
 
 function getCell(property, maxLength) {
 	if (!property) return ' '.repeat(maxLength);
 	if (property instanceof Date)
 		return Intl.DateTimeFormat('ru').format(property).replace(/\./g, '-');
-	if (property.length > maxLength) return property.slice(0, maxLength - 3) + '...';
+	if (property?.length > maxLength) return property.slice(0, maxLength - 1) + '…';
 	return property.padEnd(maxLength);
 }
 
-function getTableSizes(data) {
-	let columnSize = [1, 9, 10, 7, 4];
-	for (const record of data) {
-		if (record.name && record.name.length > columnSize[1])
-			columnSize[1] = record.name.length;
-		if (record.comment && record.comment.length > columnSize[3])
-			columnSize[3] = record.comment.length;
-		if (record.fileName && record.fileName.length > columnSize[4])
-			columnSize[4] = record.fileName.length;
-	}
-	if (columnSize[1] > 10) columnSize[1] = 10;
-	if (columnSize[3] > 50) columnSize[3] = 50;
-	if (columnSize[4] > 20) columnSize[4] = 20;
+function getTableSize(data) {
+	let tableSize = {important: 1, name: 9, dateOfCreation: 10, comment: 7, fileName: 9};
 
-	return columnSize;
+	for (const record of data) {
+		if (record.name?.length > tableSize.name)
+			tableSize.name = record.name.length;
+		if (record.comment.length > tableSize.comment)
+			tableSize.comment = record.comment.length;
+		if (record.fileName.length > tableSize.fileName)
+			tableSize.fileName = record.fileName.length;
+	}
+
+	if (tableSize.name > 10) tableSize.name = 10;
+	if (tableSize.comment > 50) tableSize.comment = 50;
+	if (tableSize.fileName > 20) tableSize.fileName = 20;
+
+	return tableSize;
 }
 
-function createTitle(tableSizes) {
+function createTitle(tableSize) {
 	console.log(getLine(
 		{important: 1, name: 'name', dateOfCreation: 'date', comment: 'comment', fileName: 'file name'},
-		tableSizes));
+		tableSize));
 }
 
-function createSeparatorLine(tableSizes) {
-	let paragraph = '  ';
-	let count = tableSizes.reduce((sum, current) => sum + current, 0) + 3 * 4 + 5;
-	console.log(paragraph + '-'.repeat(count));
+function createSeparatorLine(tableSize) {
+	let prefix = '  ';
+	let count = tableSize.important + tableSize.name + tableSize.dateOfCreation
+		+ tableSize.comment + tableSize.fileName + 4 * 3 + 5;
+	console.log(prefix + '-'.repeat(count));
 }
 
 // TODO you can do it!

78e90c7fde19db98d810c9c289cc138cceb6b593
Amur <amur_almukhametov@mail.ru>
2021-03-17 13:41:59 +0500

    "Первые 3 задания"

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const {getAllFilePathsWithExtension, readFile} = require('./fileSystem');
 const {readLine} = require('./console');
 
 const files = getFiles();
+const allTodo = getTODO();
 
 console.log('Please, write your command!');
 readLine(processCommand);
 
 function getFiles() {
-    const filePaths = getAllFilePathsWithExtension(process.cwd(), 'js');
-    return filePaths.map(path => readFile(path));
+const filePaths = getAllFilePathsWithExtension(process.cwd(), 'js');
+return filePaths.map(path => readFile(path));
 }
 
 function processCommand(command) {
@@ -16,10 +17,58 @@ function processCommand(command) {
         case 'exit':
             process.exit(0);
             break;
-        default:
-            console.log('wrong command');
+        case 'show':
+            console.log(allTodo);
             break;
+        case 'important':
+            console.log(important());
+            break;
+        case 'user':
+            console.log(user());
+            break;
+        default:
+        console.log('wrong command');
+        break;
     }
+}
+
+function getTODO(){
+    let result = [];
+        for(let file of files) {
+            let strings = file.split('\r\n').filter(x => x.includes('// TODO') && !x.includes('\'// TODO\''));
+        for(let str of strings) {
+            result.push(str.slice(str.indexOf('//')));
+        }
+    }
+    return result;
+}
+
+function important(){
+let important = [];
+
+    for (let e of allTodo){
+        if (e.includes('!')){
+        important.push(e);
+        }
+    }
+    return important;
+}
+
+
+
+function user(){
+    let user = command;
+    let ss = [];
+    let rr = allTodo.map(x => x.split(';'));
+    result1 = [];
+    
+    for(let i = 0; i < allTodo.length; i++){
+        ss.push(rr[i][0].substring(8).toUpperCase());
+        if (user === ss[i])
+            result1.push(allTodo[i]);
+    }
+    
+        return console.log(result1);
 }
 
 // TODO you can do it!

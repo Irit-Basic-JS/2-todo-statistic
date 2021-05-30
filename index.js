8d8708a45337330a576a4cf3cf2fc662f8f730c6
Sergey Miropolsky <Miropolskysk@yandex.ru>
2021-05-31 00:59:15 +0500

    Миропольский(1-4)

--- index.js
+++ index.js
@@ -12,7 +12,17 @@ function getFiles() {
 }
 
 function processCommand(command) {
-    switch (command) {
+    let commands = command.split(' ');
+    switch (commands[0]) {
+        case 'show':
+            console.log(toShow());
+            break;
+        case 'important':
+            console.log(toImportant());
+            break;
+        case 'user':
+            console.log(toUser(commands[1]));
+            break;
         case 'exit':
             process.exit(0);
             break;
@@ -20,6 +30,42 @@ function processCommand(command) {
             console.log('wrong command');
             break;
     }
+}
+
+function toShow() {
+    let result = [];
+    for (let file of files){
+        let strings = file.split("\n");
+        for (let str of strings){
+            if (str.includes("// TODO ")){
+                result.push(str.substring(str.indexOf("// TODO ")));
+            }
+        }
+    }
+    return result;
+}
+
+function toImportant(){
+    let todos = toShow();
+    let result = [];
+    for (let todo of todos){
+        if (todo.includes('!')){
+            result.push(todo);
+        }
+    }
+    return result;
+}
+
+function toUser(userName) {
+    userName = userName.toLowerCase();
+    let todos = toShow();
+    let result = [];
+    for(todo of todos) {
+        if(todo.split(';').length === 3 && todo.toLowerCase().startsWith(userName)) {
+            result.push(todo);
+        }
+    }
+    return result;
 }
 
 // TODO you can do it!

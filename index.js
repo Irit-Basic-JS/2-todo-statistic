a5a11366d0e736876db220295d608c6bb4975b31
AdIVIuH <kvashnin.2001@yandex.ru>
2021-06-01 00:41:19 +0500

    5 tasks done

--- index.js
+++ index.js
@@ -11,15 +11,101 @@ function getFiles() {
     return filePaths.map(path => readFile(path));
 }
 
+
 function processCommand(command) {
-    switch (command) {
+    let commands = command.split(' ');
+    let todos = getAllTodos();
+    let result;
+    switch (commands[0]) {
         case 'exit':
             process.exit(0);
+            break;
+        case 'show':
+            result = todos;
+            break;
+        case 'important':
+            result = getImportant(todos)
+            break;
+        case 'user':
+            result = getUserTodos(todos, commands[1]);
+            break;
+        case 'sort':
+            switch (commands[1]) {
+                case 'importance':
+                    result = sortImportance(todos);
+                    break;
+                case 'user':
+                    result = sortUser(todos);
+                    break;
+                default:
+                    result = sortDate(todos);
+                    break;
+            }
             break;
         default:
             console.log('wrong command');
             break;
     }
+
+    writeCommand(result);
+}
+
+function sortDate(todos) {
+    return todos.slice().sort((a, b) => getDate(b).localeCompare(getDate(a)));
+}
+
+function sortUser(todos) {
+    return todos.slice().sort((a, b) => getUser(a).localeCompare(getUser(b)));
+}
+
+function getImportant(todos) {
+    return todos.filter(todo => todo.includes('!'));
+}
+
+function sortImportance(todos) {
+    return todos.slice().sort((a, b) => (b.match(/!/gi)?.length || 0) - (a.match(/!/gi)?.length || 0));
+}
+
+function getAllTodos() {
+    let files = getFiles();
+    return files
+        .map(file => file
+            .split("\r\n")
+            .map(str => str.trim())
+            .filter(str =>
+                str.includes('// TODO')
+                && !str.includes("\'// TODO")
+                && !str.includes("\"// TODO")
+                && !str.includes("\`// TODO")
+            )
+            .map(str => str.slice(str.indexOf('// TODO')))
+        )
+        .flat();
+}
+
+function writeTODOs(todos) {
+    console.log("TODO list: ");
+    console.log(todos.join('\n'));
+}
+
+function getUserTodos(todos, user) {
+    return todos.filter(todo =>
+        todo.includes(';')
+        && getUser(todo).toLowerCase() === user);
+}
+
+function getUser(todo) {
+    return todo.slice('// TODO'.length, todo.indexOf(';')).trim();
+}
+
+function getDate(todo) {
+    return todo.slice(todo.indexOf(';'), todo.indexOf(';', todo.indexOf(';') + 1)).trim();
+}
+
+function writeCommand(todos) {
+    console.log("=====================================");
+    writeTODOs(todos);
+    console.log("=====================================");
 }
 
 // TODO you can do it!

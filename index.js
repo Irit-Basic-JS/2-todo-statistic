accd42e73af770a6be6fcac4e34ee71510a7415a
dclxxxvi <t.aleksandr888@mail.ru>
2021-10-06 02:46:35 +0500

    Александр Тихонов

--- index.js
+++ index.js
@@ -11,10 +11,56 @@ function getFiles() {
     return filePaths.map(path => readFile(path));
 }
 
+function getTODO() {
+    let TODOList = [];
+    let example = '// TODO';
+    for (let file of files) {
+        for (let str of file.split('\r\n')) {
+            if (str.includes(example) && !str.includes('\'' + example))
+                TODOList.push(str.slice(str.indexOf(example)));
+        }
+    }
+    return TODOList;
+}
+
+function getImportant() {
+    return getTODO().filter((str) => str.includes('!'));
+}
+
+function getUser(user) {
+    result = [];
+    for (let str of getTODO().filter((str) => (str.includes(';')))) {
+        let i = 8;
+        for (let j = 0; j < user.length; j++) {
+            if (user[j].toLowerCase() == str[i].toLowerCase()) {
+                i++;
+            }
+            else break;
+        }
+        if (i == 8 + user.length) {
+            result.push(str);
+        }
+    }
+    return result;
+}
+
 function processCommand(command) {
-    switch (command) {
+    let commands = command.split(' ');
+    switch (commands[0]) {
         case 'exit':
             process.exit(0);
+            break;
+        case 'show':
+            console.log(getTODO());
+            break;
+        case 'important':
+            console.log(getImportant());
+            break;
+        case 'user':
+            console.log(getUser(commands[1]));
+            break;
+        case 'sort':
+            console.log();
             break;
         default:
             console.log('wrong command');

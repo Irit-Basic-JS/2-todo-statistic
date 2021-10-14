53c4759791178c31ca00b7fbb75dd33f90f17c07
AnesVijay <2011andreyka@gmail.com>
2021-10-14 22:50:03 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const {getAllFilePathsWithExtension, readFile} = require('./fileSystem');
 const {readLine} = require('./console');
+//const { fstat } = require('fs');
 
 const files = getFiles();
-
+const comments = getAllComments();
 console.log('Please, write your command!');
 readLine(processCommand);
+
 
 function getFiles() {
     const filePaths = getAllFilePathsWithExtension(process.cwd(), 'js');
@@ -12,14 +14,38 @@ function getFiles() {
 }
 
 function processCommand(command) {
+    //const commandParameters = command.split(' ');
+    //let comms = [];
     switch (command) {
         case 'exit':
             process.exit(0);
+        case 'show':
+            console.log(comments);
             break;
+        case 'important':
+            console.log(comments.filter(x => x.important));
+            break;
+        case 'user':
+            console.log(comments.filter(x => x.user));
         default:
             console.log('wrong command');
             break;
     }
 }
+
+function getAllComments() {
+    let lines = [];
+    for (let file of files)
+        lines.push(...file.split('\r\n'));
+    lines = lines.flat(Infinity);
+    const matches = [];
+    for (let line of lines) {
+        let search = line.indexOf(`// TODO`);
+        if (search != -1)
+            matches.push(line.substring(search));
+    }
+    return matches;   
+}
+
 
 // TODO you can do it!
